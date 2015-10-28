b5f9aab0c7e1134ee6664406255414213761e5c2
jhernandezn <sidlors@github.com>
2015-10-27 21:17:37 -0600

    se agregan clases para hacer pruebas unitarias con tomEE embebbed

---
+++ src/main/java/mx/com/sidlors/jpa/entity/BooleanConverter.java
@@ -0,0 +1,30 @@
+package mx.com.sidlors.jpa.entity;
+//package jpa.entity;
+//
+//import javax.persistence.AttributeConverter;
+//import javax.persistence.Converter;
+//
+//@Converter
+//public class BooleanConverter implements AttributeConverter<Boolean, Integer> {
+//
+//	public Integer convertToDatabaseColumn(Boolean attribute) {
+//		if (Boolean.TRUE.equals(attribute)) {
+//			return 1;
+//		} else {
+//			return -1;
+//		}
+//	}
+//
+//	public Boolean convertToEntityAttribute(Integer dbData) {
+//		if (dbData == null) {
+//			return Boolean.FALSE;
+//		} else {
+//			if (dbData == 1) {
+//				return Boolean.TRUE;
+//			} else {
+//				return Boolean.FALSE;
+//			}
+//		}
+//	}
+//
+//}
